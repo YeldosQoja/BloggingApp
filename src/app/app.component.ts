6b8fe69af75ee44b33b6d16fe6420005e0cde659
YeldosQoja <yeldos.kozha@gmail.com>
2024-05-19 16:29:23 +0500

    Add top bar component

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,11 +2,14 @@ import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { LoginComponent } from './login/login.component';
 import { RegisterComponent } from './register/register.component';
+import { HomeComponent } from './home/home.component';
+import { TopBarComponent } from './top-bar/top-bar.component';
+import { BlogDetailComponent } from './blog-detail/blog-detail.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, LoginComponent, RegisterComponent],
+  imports: [RouterOutlet, TopBarComponent, LoginComponent, RegisterComponent, HomeComponent, BlogDetailComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
